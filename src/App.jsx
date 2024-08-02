4460510491303565247da988e9992c920c69e78e
Collins7-7 <collinsmuiruri77@gmail.com>
2024-08-02 16:43:18 +0300

    Done with navbar UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Home from './pages/home/Home'
+import Navbar from './pages/navbar/Navbar'
 
 function App() {
  
 
   return (
     <>
+    <Navbar></Navbar>
       <Home></Home>
     </>
   )
